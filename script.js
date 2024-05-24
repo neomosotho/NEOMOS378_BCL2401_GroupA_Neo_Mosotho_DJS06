35a599c8df8fe13cf52657e531d7d5edf4200a7c
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-24 16:07:06 +0200

    Advanced exercises

--- script.js
+++ script.js
@@ -25,34 +25,71 @@ provinces.forEach (province =>{
     console.log(province)
   })
 
-  //Logging provinces for each name
-names.forEach ((name, province) => {
-    console.log('${name} (${provinces[index]})')
+//Logging provinces for each name
+names.forEach ((name, index) => {
+    console.log(`${name} (${provinces[index]})`)
   })
 
-  //Exercise 2
+//Exercise 2
   const provinceUppercase = provinces.map(province => province.toUpperCase());
   console.log(provinceUppercase);
 
-  //Exercise 3
-const nameLengths = names.map(name => name.length());
+//Exercise 3
+const nameLengths = names.map(name => name.length);
 console.log(nameLengths);
 
-  //Exercise 4
+//Exercise 4
   const sortedProvinces = [...provinces].sort();
   console.log(sortedProvinces);
 
-  //Exercise 5
+//Exercise 5
 const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
 console.log(filteredProvinces);
 
-  //Exercise 6
+//Exercise 6
   const containsS = names.map(name => name.includes('S') || name.includes('s'));
   console.log(containsS);
 
-  //Exercise 7
+//Exercise 7
 const nameProvinceMapping = names.reduce((acc, name, index) => {
   acc[name] = provinces[index];
   return acc;
 }, {});
 console.log(nameProvinceMapping);
+
+//Advanced Exercises 
+//Exercise 1
+console.log(products.map(({ product }) => product));
+
+//Exercise 2
+console.log(products.filter(({ product }) => product.length <= 5));
+
+//Exercise 3
+console.log(products
+    .filter(({ price }) => !isNaN(price) && price !== '' && price.trim() !== '')
+    .reduce((total, { price }) => total + Number(price), 0)
+  );
+
+//Exercise 4
+console.log(products.reduce((acc, { product }) => acc + product, ''));
+
+//Exercise 5
+console.log(products
+    .filter(({ price }) => !isNaN(price) && price !== '' && price.trim() !== '')
+    .reduce((acc, { product, price }) => {
+      const numericPrice = Number(price);
+      if (numericPrice > acc.highest.price) {
+        acc.highest = { product, price: numericPrice };
+      }
+      if (numericPrice < acc.lowest.price) {
+        acc.lowest = { product, price: numericPrice };
+      }
+      return acc;
+    }, {
+      highest: { product: '', price: -Infinity },
+      lowest: { product: '', price: Infinity }
+    })
+  );
+
+//Exercise 6
+console.log(products.map(({ product, price }) => ({ name: product, cost: price })));
